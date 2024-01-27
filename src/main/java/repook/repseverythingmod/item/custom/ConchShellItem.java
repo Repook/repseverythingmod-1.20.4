8cf81a7cafd4cfc654af7cf50ad1b437aa5a0976
Repook <supercoopergaming123@gmail.com>
2024-01-27 21:37:05 +1300

    all day 10 stuff

--- src/main/java/repook/repseverythingmod/item/custom/ConchShellItem.java
+++ src/main/java/repook/repseverythingmod/item/custom/ConchShellItem.java
@@ -14,6 +14,7 @@ import net.minecraft.util.UseAction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
@@ -24,7 +25,6 @@ import repook.repseverythingmod.sound.ModSounds;
 
 
 public class ConchShellItem extends Item {
-
 
 
 
